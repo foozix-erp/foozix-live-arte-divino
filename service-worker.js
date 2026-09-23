b9b59d9a31bf3b9c7ca76500fe809053a878706e
FOOZIX <foozix.erp@gmail.com>
2026-09-23 13:48:04 +0300

    Improve mobile PWA branding and installation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='foozix-live-v3.2.0';
+const CACHE='foozix-live-v3.3.0';
 const SHELL=['./','./index.html','./app.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
